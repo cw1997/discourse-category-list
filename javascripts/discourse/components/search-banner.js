5017bace42222f102e85f35a15ca697fcbb71035
Chang Wei <changwei1006@gmail.com>
2023-02-10 11:50:48 +0800

    update display for route rule

--- javascripts/discourse/components/search-banner.js
+++ javascripts/discourse/components/search-banner.js
@@ -1,7 +1,7 @@
 import Component from "@ember/component";
 import { inject as service } from "@ember/service";
-import { defaultHomepage } from "discourse/lib/utilities";
-import { and } from "@ember/object/computed";
+// import { defaultHomepage } from "discourse/lib/utilities";
+// import { and } from "@ember/object/computed";
 import discourseComputed, { observes } from "discourse-common/utils/decorators";
 
 export default Component.extend({
@@ -10,38 +10,10 @@ export default Component.extend({
 
   @discourseComputed("router.currentRouteName")
   displayForRoute(currentRouteName) {
-    const showOn = settings.show_on;
-    if (showOn === "homepage") {
-      return currentRouteName === `discovery.${defaultHomepage()}`;
-    } else if (showOn === "top_menu") {
-      return this.siteSettings.top_menu
-        .split("|")
-        .any((m) => `discovery.${m}` === currentRouteName);
-    } else if (showOn === "latest") {
-      return currentRouteName === `latest`;
-    } else {
-      // "all"
-      return (
-        currentRouteName !== "full-page-search" &&
-        !currentRouteName.startsWith("admin.")
-      );
-    }
+    return currentRouteName === "discovery.latest" // && !currentRouteName.startsWith("admin.")
   },
 
-  @discourseComputed("currentUser")
-  displayForUser(currentUser) {
-    const showFor = settings.show_for;
-    if (showFor === "everyone") {
-      return true;
-    } else if (showFor === "logged_out" && !currentUser) {
-      return true;
-    } else if (showFor === "logged_in" && currentUser) {
-      return true;
-    }
-    return false;
-  },
-
-  shouldDisplay: and("displayForUser", "displayForRoute"),
+  shouldDisplay: "displayForRoute",
 
   // Setting a class on <html> from a component is not great
   // but we need it for backwards compatibility
